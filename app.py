58a8ab855086960ae485fc5d16b2ac2e211853d9
Juhasz Szabolcs PG <juhasz.szabolcs@szbi-pg.hu>
2024-10-14 22:34:27 +0200

    Add

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    product_name = None
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+    return render_template('index.html', product=product_name)
 
 
 if __name__ == "__main__":
