4635e02ea90a46ac17261b832ecc5dd0ba8499e6
Juhasz Szabolcs PG <juhasz.szabolcs@szbi-pg.hu>
2024-10-14 22:53:55 +0200

    Implement dynamic product list and form handling

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+products = []
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    product_name = None
     if request.method == 'POST':
         product_name = request.form['product_name']
-    return render_template('index.html', product=product_name)
+        if product_name:
+            products.append(product_name)
+    return render_template('index.html', products_=products)
 
 
 if __name__ == "__main__":
