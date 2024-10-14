e8e49af34cea228b56760048edd050d3799ecdeb
Juhasz Szabolcs PG <juhasz.szabolcs@szbi-pg.hu>
2024-10-14 23:43:21 +0200

    feat: add products submission and display functionality on products.html

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 products = []
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods = ['GET'])
 def index():
-    if request.method == 'POST':
-        product_name = request.form['product_name']
-        if product_name:
-            products.append(product_name)
-    return render_template('index.html', products_=products)
+    return render_template('index.html')
 
+@app.route('/submit_products', methods = ['POST'])
+def submit_products():
+    product_name = request.form.get('product_name')
+    if product_name:
+        products.append(product_name)
+    return redirect(url_for('index'))
+
+@app.route('/products')
+def get_products():
+    return render_template('products.html', products=products)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
